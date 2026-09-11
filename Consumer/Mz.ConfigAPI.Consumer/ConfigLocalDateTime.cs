1de494bed63c9ca5dab6775d3c1b0b481ca13e93
Marco-Zechner <marcoflorian00956@gmail.com>
2026-09-12 01:57:31 +0200

    split ConfigValue.cs file

---
+++ Consumer/Mz.ConfigAPI.Consumer/ConfigLocalDateTime.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Mz.ConfigApi
+{
+    public sealed class ConfigLocalDateTime : IEquatable<ConfigLocalDateTime>
+    {
+        public ConfigLocalDateTime(ConfigDate date, ConfigTime time)
+        {
+            if (date == null)
+                throw new ArgumentNullException(nameof(date));
+
+            if (time == null)
+                throw new ArgumentNullException(nameof(time));
+
+            Date = date;
+            Time = time;
+        }
+
+        public ConfigDate Date { get; }
+        public ConfigTime Time { get; }
+
+        public bool Equals(ConfigLocalDateTime other) 
+            => other != null && Date.Equals(other.Date) && Time.Equals(other.Time);
+
+        public override bool Equals(object obj) => Equals(obj as ConfigLocalDateTime);
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ( Date.GetHashCode() * 397 ) ^ Time.GetHashCode();
+            }
+        }
+    }
+}
---
+++ Consumer/Mz.ConfigAPI.Consumer/ConfigTime.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Mz.ConfigApi
+{
+    public sealed class ConfigTime : IEquatable<ConfigTime>
+    {
+        public ConfigTime(int hour, int minute, int second) : this(hour, minute, second, string.Empty) { }
+
+        public ConfigTime(int hour, int minute, int second, string fractionalSeconds)
+        {
+            if (hour < 0 || hour > 23 || minute < 0 || minute > 59 || second < 0 || second > 60)
+                throw new ArgumentException("The supplied components do not form a valid local time.");
+
+            if (fractionalSeconds == null)
+                throw new ArgumentNullException(nameof(fractionalSeconds));
+
+            foreach (char c in fractionalSeconds)
+            {
+                if (c < '0' || c > '9')
+                    throw new ArgumentException("Fractional seconds must contain only digits.", nameof(fractionalSeconds));
+            }
+
+            Hour = hour;
+            Minute = minute;
+            Second = second;
+            FractionalSeconds = fractionalSeconds;
+        }
+
+        public int Hour { get; }
+        public int Minute { get; }
+        public int Second { get; }
+        public string FractionalSeconds { get; }
+
+        public bool Equals(ConfigTime other) 
+            => other != null && Hour == other.Hour && Minute == other.Minute && Second == other.Second &&
+               string.Equals(FractionalSeconds, other.FractionalSeconds, StringComparison.Ordinal);
+
+        public override bool Equals(object obj) => Equals(obj as ConfigTime);
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = Hour;
+                hash = ( hash * 397 ) ^ Minute;
+                hash = ( hash * 397 ) ^ Second;
+                hash = ( hash * 397 ) ^ StringComparer.Ordinal.GetHashCode(FractionalSeconds);
+
+                return hash;
+            }
+        }
+    }
+}
+
